5a51cdb00fd9d81027942ba96b2e43854e58b481
Senna03-n <s3929517@rmit.edu.vn>
2025-10-17 15:48:29 +1100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let mouseY = window.innerHeight / 2;
 let isMoving = false;
 let moveTimeout;
 
-
 document.addEventListener('mousemove', function(e) {
   if (eyesVisible) {
     const mouseX = e.clientX;
@@ -33,27 +32,6 @@ function showEyes() {
     eyesVisible = true;
   }
 }
-
-
-document.addEventListener('mousemove', function(e) {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
- 
-    eyes.forEach(eye => {
-      const pupil = eye.querySelector('.pupil');
-      const eyeRect = eye.getBoundingClientRect();
-      const eyeCenterX = eyeRect.left + eyeRect.width / 2;
-      const eyeCenterY = eyeRect.top + eyeRect.height / 2;           
-      const angle = Math.atan2(mouseY - eyeCenterY, mouseX - eyeCenterX);
-      const distance = Math.min(eyeRect.width * 0.15, 15);
-      const pupilX = Math.cos(angle) * distance;
-      const pupilY = Math.sin(angle) * distance;
-      pupil.style.transform = `translate(calc(-50% + ${pupilX}px), calc(-50% + ${pupilY}px))`;
-    });
-    
-});
-
-
 
 function hideEyes() {
   if (eyesVisible) {
